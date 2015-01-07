2a79f868de20bd963e767c53530e6d7630ee020f
jsbed <bedardjs97@hotmail.com>
2015-01-07 17:25:43 -0500

    added some event handlers for the buttons

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,30 @@
 $( document ).ready(function() {
-    console.log( "ready!" );
+
+    $("#button1").click(function(event){
+        $('#button1').prop('disabled', true);
+    });
+
+    $("#button2").click(function(event){
+        $('#button2').prop('disabled', true);
+    });
+
+    $("#button3").click(function(event){
+        $('#button3').prop('disabled', true);
+    });
+
+    $("#button4").click(function(event){
+        $('#button4').prop('disabled', true);
+    });
+
+    $("#button5").click(function(event){
+        $('#button5').prop('disabled', true);
+    });
+
+    $("#reset").click(function(event){
+        $('#button1').prop('disabled', false);
+        $('#button2').prop('disabled', false);
+        $('#button3').prop('disabled', false);
+        $('#button4').prop('disabled', false);
+        $('#button5').prop('disabled', false);
+    });
 });
