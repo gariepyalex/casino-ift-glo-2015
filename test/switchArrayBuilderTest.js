903313eb8317a54dd2fc270c87c5c13991bea639
Alexandre Gariépy <gariepy.alex@gmail.com>
2015-01-07 16:01:06 -0500

    The callback function is called properly on explosion

--- test/switchArrayBuilderTest.js
+++ test/switchArrayBuilderTest.js
@@ -13,8 +13,22 @@ describe("SwitchArrayBuilder", function() {
     });
 
     it("you can specify the number of switches", function() {
-        switchArray = builder.withNumberOfSwitches(3).build();
+        var switchArray = builder.withNumberOfSwitches(3).build();
         assert.equal(switchArray.length, 3);
     });
 
+    it("the callback is called only when pressing a single switch", function() {
+        var callback = sinon.spy();
+        var switchArray = builder.withNumberOfSwitches(4).withBombCallback(callback).build();
+
+        switchArray[0].press();
+        switchArray[1].press();
+        switchArray[2].press();
+        switchArray[3].press();
+
+        assert(callback.calledOnce);
+    });
+
 });
+
+
