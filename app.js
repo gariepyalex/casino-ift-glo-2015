7ae414c56e88625d544451aaa970da1868954643
Alexandre Gariépy <gariepy.alex@gmail.com>
2015-01-08 00:59:09 -0500

    Moins de modules, mais c'est moins pété

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ var app = express();
 var http = require('http');
 var httpServer = http.Server(app);
 var io = require('socket.io')(httpServer);
-var GameSocketCommunication = require("./gameSocketCommunication.js");
-var gameSocket = new GameSocketCommunication(io, http);
+var Game = require("./game.js");
+var game = new Game(explosionCallback);
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use("/css",  express.static(__dirname + '/public/css'));
@@ -17,7 +17,26 @@ app.get('/', function(req, res) {
   res.sendFile(__dirname + '/public/index.html');
 });
 
-io.on("connection", gameSocket.onConnection);
+
+io.on("connection", function(socket) {
+        console.log("user connected");
+        socket.emit("game current state", game.getStateJSON());
+
+        socket.on("game reset", function() {
+            game.reset();
+            io.emit("game current state", game.getStateJSON());
+        });
+
+        socket.on("game press switch", function(id) {
+            game.pressSwitch(id);
+            io.emit("game current state", game.getStateJSON());
+        });
+});
+
+function explosionCallback() {
+    console.log("boom");
+    io.emit("game explosion boom");
+};
 
 httpServer.listen(PORT, function(){
   console.log("listening on port: " + PORT);
