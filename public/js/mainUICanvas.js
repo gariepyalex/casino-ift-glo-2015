b8bb63439aeb4bae7ae860cda8ee1a2fb59a599e
Alexandre Gariépy <gariepy.alex@gmail.com>
2015-01-13 17:17:46 -0500

    Now differents

--- public/js/mainUICanvas.js
+++ public/js/mainUICanvas.js
@@ -4,7 +4,11 @@ $(document).ready(function() {
     stage = new createjs.Stage("stageCanvas");
 
     var manifest = [
-        {src: "../img/detonator/red_detonator_sprite.png", id: "detonator"}
+        {src: "../img/detonator/red_detonator_sprite.png", id: "red_detonator"},
+        {src: "../img/detonator/yellow_detonator_sprite.png", id: "yellow_detonator"},
+        {src: "../img/detonator/blue_detonator_sprite.png", id: "blue_detonator"},
+        {src: "../img/detonator/green_detonator_sprite.png", id: "green_detonator"},
+        {src: "../img/detonator/purple_detonator_sprite.png", id: "purple_detonator"}
     ];
 
     loader = new createjs.LoadQueue(false);
@@ -13,27 +17,60 @@ $(document).ready(function() {
 });
 
 function handleComplete() {
-    var spriteSheet = new createjs.SpriteSheet({
-        framerate: 12,
-        "images": [loader.getResult("detonator")],
+    var spriteSheetRed = new createjs.SpriteSheet({
+        framerate: 5,
+        "images": [loader.getResult("red_detonator")],
+        "frames": {"width": 40, "height": 70, "regX": 0, "regY": 0},
+        "animations": {
+            "detonation": [0, 5, "detonation"]
+        }
+    });
+    var spriteSheetYellow = new createjs.SpriteSheet({
+        framerate: 5,
+        "images": [loader.getResult("yellow_detonator")],
+        "frames": {"width": 40, "height": 70, "regX": 0, "regY": 0},
+        "animations": {
+            "detonation": [0, 5, "detonation"]
+        }
+    });
+    var spriteSheetBlue = new createjs.SpriteSheet({
+        framerate: 5,
+        "images": [loader.getResult("blue_detonator")],
+        "frames": {"width": 40, "height": 70, "regX": 0, "regY": 0},
+        "animations": {
+            "detonation": [0, 5, "detonation"]
+        }
+    });
+    var spriteSheetGreen = new createjs.SpriteSheet({
+        framerate: 5,
+        "images": [loader.getResult("green_detonator")],
+        "frames": {"width": 40, "height": 70, "regX": 0, "regY": 0},
+        "animations": {
+            "detonation": [0, 5, "detonation"]
+        }
+    });
+    var spriteSheetPurple = new createjs.SpriteSheet({
+        framerate: 5,
+        "images": [loader.getResult("purple_detonator")],
         "frames": {"width": 40, "height": 70, "regX": 0, "regY": 0},
         "animations": {
             "detonation": [0, 5, "detonation"]
         }
     });
 
-    var animation1 = new createjs.Sprite(spriteSheet, "detonation");
-    var animation2 = new createjs.Sprite(spriteSheet, "detonation");
-    var animation3 = new createjs.Sprite(spriteSheet, "detonation");
-    var animation4 = new createjs.Sprite(spriteSheet, "detonation");
-    var animation5 = new createjs.Sprite(spriteSheet, "detonation");
-    animation1.x = 100; animation1.y = 100;
-    animation2.x = 200; animation2.y = 100;
-    animation3.x = 300; animation3.y = 100;
-    animation4.x = 400; animation4.y = 100;
-    animation5.x = 500; animation5.y = 100;
+    var redDetonator = new createjs.Sprite(spriteSheetRed, "detonation");
+    var yellowDetonator = new createjs.Sprite(spriteSheetYellow, "detonation");
+    var blueDetonator = new createjs.Sprite(spriteSheetBlue, "detonation");
+    var greenDetonator = new createjs.Sprite(spriteSheetGreen, "detonation");
+    var purpleDetonator = new createjs.Sprite(spriteSheetPurple, "detonation");
 
-    stage.addChild(animation1, animation2, animation3, animation4, animation5);
+    redDetonator.x = 100; redDetonator.y = 100;
+    yellowDetonator.x = 200; yellowDetonator.y = 100;
+    blueDetonator.x = 300; blueDetonator.y = 100;
+    greenDetonator.x = 400; greenDetonator.y = 100;
+    purpleDetonator.x = 500; purpleDetonator.y = 100;
+
+    stage.addChild(redDetonator, yellowDetonator, blueDetonator, greenDetonator, purpleDetonator);
 
     createjs.Ticker.timingMode = createjs.Ticker.RAF;
     createjs.Ticker.addEventListener("tick", tick);
