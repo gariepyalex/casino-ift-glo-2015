0c69948c36aed3e843ce332542f0bddde784a6e3
Matiel <mathieu_carpentier@hotmail.com>
2015-01-16 22:24:44 -0500

    Added playerList in /game

--- public/js/mainUI.js
+++ public/js/mainUI.js
@@ -10,6 +10,7 @@ $( document ).ready(function() {
 
     socket.on("game current state", function(state) {
         console.log(state);
+        updatePlayerList(state);
         if(state["SWITCHES"]){
             renderer.setSwitchState(state["SWITCHES"]);
         }
@@ -19,10 +20,40 @@ $( document ).ready(function() {
     });
 
     var bindClicks = function() {
-        $("#reset").unbind("click");
-        $("#reset").click(function(){
+        var reset =  $("#reset");
+       reset.unbind("click");
+        reset.click(function(){
             socket.emit("game reset");
         });
+    };
+
+    var updatePlayerList = function(state){
+        var i = 1;
+        clearPlayerList();
+        _.each(state.PLAYER_QUEUE, function(player){
+            addPlayerToListAtPosition(player, i);
+            i = i + 1
+        });
+    };
+
+    var addPlayerToListAtPosition = function(player, position){
+        var playerPosition = $("#playerPosition" + position);
+        var playerName = playerPosition.children(".playerName");
+        var playerImage = playerPosition.children("img");
+        playerName.text(player.NAME);
+        playerImage.removeClass();
+        playerImage.addClass("playerImage");
+        playerImage.addClass("D" + player.ID.toString())
+    };
+
+    var clearPlayerList = function(){
+        for (var i = 1; i <= 3; i++){
+            var playerPosition = $("#playerPosition" + i);
+            var playerName = playerPosition.children(".playerName");
+            var playerImage = playerPosition.children("img");
+            playerName.text("");
+            playerImage.removeClass();
+        }
     };
 
     bindClicks();
